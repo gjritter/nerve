3c2b8432891d9595a1bc8f610493fa75f0b217dc
Greg Ritter <gjritter@codespin.com>
2009-12-07 20:15:18 -0700

    Replaced send_html with a respond method that can take a string or an object.

--- nerve.js
+++ nerve.js
@@ -18,9 +18,11 @@ del = function(regexp) {
 };
 
 (function() {
-	function send_html(content, status_code) {
-		this.sendHeader(status_code || 200, {"Content-Type":"text/html","Content-Length":content.length});
-		this.sendBody(content);
+	function respond(response_data) {
+		var headers = {"Content-Type": "text/html", "Content-Length": (response_data.content && response_data.content.length) || response_data.length || 0}
+		for(name in response_data.headers) { headers[name] = response_data.headers[name]; }
+		this.sendHeader(response_data.status_code || 200, headers);
+		this.sendBody(response_data.content || response_data);
 		this.finish();
 	}
 
@@ -32,7 +34,7 @@ del = function(regexp) {
 
 	function create(app) {
 		function request_handler(req, res) {
-			res.send_html = send_html;
+			res.respond = respond;
 			for(var i = 0; i < app.length; i++) {
 				var matcher = app[i][0], handler = app[i][1],
 					match = req.uri.path.match(is_regexp(matcher) ? matcher : matcher.apply(req));
@@ -40,18 +42,24 @@ del = function(regexp) {
 					try {
 						handler.apply(null, [req, res].concat(match.slice(1)));
 					} catch(e) {
-						res.send_html('<html><head><title>Exception</title></head><body><h1>Exception</h1><pre>' + sys.inspect(e) + '</pre></body></html>', 501);
+						res.respond({content: '<html><head><title>Exception</title></head><body><h1>Exception</h1><pre>' + sys.inspect(e) + '</pre></body></html>', status_code: 501});
 					}
 					return;
 				}
 			}
-			res.send_html('<html><head><title>Not Found</title></head><body><h1>Not Found</h1></body></html>', 404);
+			res.respond({content: '<html><head><title>Not Found</title></head><body><h1>Not Found</h1></body></html>', status_code: 404});
 		}
 
+		var server = http.createServer(request_handler);
+		
 		return {
 			serve: function(port, host) {
-				http.createServer(request_handler).listen(port, host);
-				
+				server.listen(port, host);
+				return this;
+			},
+			
+			close: function() {
+				server.close();
 				return this;
 			}
 		}
