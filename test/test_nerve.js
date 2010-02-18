87c7eec7db5af16f41b238700980024aa512c336
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-02-18 14:33:33 +0800

    Made changes to work with node v0.1.29 API changes
    Fixed ServerResponse to use write() instead of sendBody() and close() instead of finish().
    Posix module has been renamed to fs. posix.cat is fs.readFile
    
    Also fixed tests to use the response listener on ClientRequest and send an explicit close()

--- test/test_nerve.js
+++ test/test_nerve.js
@@ -24,10 +24,10 @@
 	
 	function assert_response(response, expected_body, callback) {
 		var body = '';
-		response.addListener('body', function (chunk) {
+		response.addListener('data', function (chunk) {
 			body += chunk;
 		});
-		response.addListener('complete', function () {
+		response.addListener('end', function () {
 			test.assertEquals(expected_body, body);
 		});
 		if (typeof callback === 'function') {
@@ -73,257 +73,284 @@
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('GET', '/');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			test.assertEquals(200, res.statusCode);
 			test.assertEquals('text/html', res.headers['content-type']);
 			assert_response(res, 'Hello, World!');
 		});
+        req.close();
 	}());
 	
 	(function test_unmatched_request_string() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('GET', '/unmatched');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function (res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_get_to_get_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('GET', '/get');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			test.assertEquals(200, res.statusCode);
 			test.assertEquals('text/html', res.headers['content-type']);
 			assert_response(res, 'GET matcher');
 		});
+        req.close();
 	}());
 	
 	(function test_post_to_get_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('POST', '/get');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_put_to_get_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('PUT', '/get');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_delete_to_get_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('DELETE', '/get');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_get_to_get_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('GET', '/getstring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			test.assertEquals(200, res.statusCode);
 			test.assertEquals('text/html', res.headers['content-type']);
 			assert_response(res, 'GET string matcher');
 		});
+        req.close();
 	}());
 	
 	(function test_post_to_get_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('POST', '/getstring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_put_to_get_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('PUT', '/getstring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_delete_to_get_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('DELETE', '/getstring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_nonmatching_get_to_get_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('GET', '/getstring2');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_post_to_post_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('POST', '/poststring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			test.assertEquals(200, res.statusCode);
 			test.assertEquals('text/html', res.headers['content-type']);
 			assert_response(res, 'POST string matcher');
 		});
+        req.close();
 	}());
 	
 	(function test_get_to_post_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('GET', '/poststring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_put_to_post_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('PUT', '/poststring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_delete_to_post_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('DELETE', '/poststring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_nonmatching_post_to_post_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('POST', '/poststring2');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_put_to_put_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('PUT', '/putstring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			test.assertEquals(200, res.statusCode);
 			test.assertEquals('text/html', res.headers['content-type']);
 			assert_response(res, 'PUT string matcher');
 		});
+        req.close();
 	}());
 	
 	(function test_get_to_put_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('GET', '/putstring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_post_to_put_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('POST', '/putstring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_delete_to_put_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('DELETE', '/putstring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_nonmatching_put_to_put_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('PUT', '/putstring2');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_del_to_del_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('DELETE', '/delstring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			test.assertEquals(200, res.statusCode);
 			test.assertEquals('text/html', res.headers['content-type']);
 			assert_response(res, 'DEL string matcher');
 		});
+        req.close();
 	}());
 	
 	(function test_get_to_del_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('GET', '/delstring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_post_to_del_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('POST', '/delstring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_put_to_del_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('PUT', '/delstring');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_nonmatching_del_to_del_string_matcher() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('DELETE', '/delstring2');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			assert_not_found(res);
 		});
+        req.close();
 	}());
 	
 	(function test_static_file() {
 		var client = http.createClient(8000, '127.0.0.1'),
 			req = client.request('GET', '/test.html');
 		expect_callback();
-		req.finish(function (res) {
+		req.addListener( 'response', function(res) {
 			test.assertEquals(200, res.statusCode);
 			test.assertEquals('text/html', res.headers['content-type']);
 			assert_response(res, 'hello world\n');
 		});
+        req.close();
 	}());
 	
 	// assert that all callbacks were called within the alloted time and exit
